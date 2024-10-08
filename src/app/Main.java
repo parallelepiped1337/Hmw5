241957cae851a9068c7c2a2c94a7e28600adc8c4
parallelepiped1337 <andrejbuiukli@gmail.com>
2024-10-08 18:18:23 +0300

    Hmw5

---
+++ src/app/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int totalSum = 0;
+
+        for (int i = 1; i <= 6; i++) {
+            totalSum += i;
+
+            System.out.println(i + ") Num is " + i + ", sum is " + totalSum);
+
+        }
+
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is " + totalSum);
+
+    }
+
+}
